78a4298a1c399a3e3f485c8bb4d115315db6a8d6
unknown <sn301620@gmail.com>
2024-02-07 09:04:33 +0700

    perpustakaan

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="css/Style.css">
+<title>Perpustakaan Online</title>
+</head>
+<body>
+
+<div id="container">   
+<div class="header"><img src="images/logo.png" width="100" height="100"><h1>Perpustakaan Universitas BSI</h1>
+</div>
+<h4>Selamat Datang, Tiyo</h4>
+
+<div class="main">
+<div class="left">
+<h3 align="center">MENU</h3>
+<ul>
+<li><a href="http://localhost/perpus/index.php">Logout</a></li>
+<li><a href="#">Home</a></li>
+<li><a href="http://localhost/perpus/daftar_buku.php">Daftar Buku</a></li>
+</ul>
+</div>
+
+<div class="middle">
+<h3 align="center">Artikel/Berita/Buku Terbaru</h3>
+<h2 align="center">Buku: Rahvayana - Aku Lala Padamu</h2><br>
+<div align="center">
+<img src="images/rahvayana.jpg" width="240" >
+</div>
+<p><a href="#">Baca Selengkapnya >></a></p>
+</div>
+
+<div class="right">
+<h3 align="center">BUKU TERPOPULER</h3>
+<ul>
+<li><a href="#">Web Design</a></li>
+<li><a href="#">Pemrograman</a></li>
+<li><a href="#">Database</a></li>
+</ul>
+</div>
+
+</div>
+</div>
+<div class="footer"><p align="center">Copyright © 2018 - Belajar CSS Responsive</a></p>
+</div>
+</div>
+</body>
+</html>
